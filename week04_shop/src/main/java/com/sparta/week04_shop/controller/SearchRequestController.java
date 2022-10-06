0b79e102900873cf1234765fb564724eeed8906a
Suyoung Yun <8904su@gmail.com>
2022-10-06 13:31:23 +0900

    NaverShopSearch controller와 service로 분리

--- week04_shop/src/main/java/com/sparta/week04_shop/controller/SearchRequestController.java
+++ week04_shop/src/main/java/com/sparta/week04_shop/controller/SearchRequestController.java
@@ -2,22 +2,24 @@ package com.sparta.week04_shop.controller;
 
 import com.sparta.week04_shop.NaverShopSearch;
 import com.sparta.week04_shop.domain.ItemDto;
+import com.sparta.week04_shop.service.ItemSearchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 public class SearchRequestController {
 
-    private final NaverShopSearch naverShopSearch;
+    private final ItemSearchService itemSearchService;
 
     @GetMapping("/api/search") // http://localhost:8080/api/search?query="아이폰"
-    public List<ItemDto> getItems(@RequestParam String query){
-        String resultString = naverShopSearch.search(query);
-        return naverShopSearch.fromJSONtoItems(resultString);
+    public List<ItemDto> getItems(@RequestParam String query) throws IOException {
+        List<ItemDto> itemDtoList = itemSearchService.getItems(query);
+        return itemDtoList;
     }
 }
