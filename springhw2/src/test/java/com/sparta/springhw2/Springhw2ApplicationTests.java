8337965632875c137821f9e41dc846808a8c3551
Suyoung Yun <8904su@gmail.com>
2022-10-10 16:26:46 +0900

    no message

---
+++ springhw2/src/test/java/com/sparta/springhw2/Springhw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springhw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springhw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
